afe8512f1654cc4f78cf5b298519d9dce06de0c2
sishriva <siddharth.shrivastava@colorado.edu>
2023-04-09 13:30:37 -0700

    Jenkins commit

--- netman_netconf_obj2.py
+++ netman_netconf_obj2.py
@@ -72,8 +72,8 @@ if __name__ == "__main__":
     for i in range(0, 5):
         connection = manager.connect(host=MGM_IP[i],
                                      port=22,
-                                     username='lab',
-                                     password='lab123',
+                                     username='netman',
+                                     password='netman',
                                      hostkey_verify=False,
                                      device_params={'name': 'iosxr'},
                                      allow_agent=False,
