b984dc2e19118d979443cdd5f00cf8ef0fae0cce
sishriva <siddharth.shrivastava@colorado.edu>
2023-04-10 15:12:57 -0700

    Jenkins commit

--- checkPEP8.py
+++ checkPEP8.py
@@ -3,16 +3,14 @@
 import subprocess
 import os
 
-def check():
-    result = subprocess.check_output("python3 -m pylint /home/netman/Documents/Lab9/netman_netconf_obj2.py | grep \'Your code has been rated\' | awk \'{print $7}\' | awk -F/ \'{print $1}\'",shell=True)
-    result = float(result)
-    if result > 10:
-        print("PEP8 style code violation detected. Exiting....")
-        os.system("exit 1")
-    else:
-        print("Code complies with the PEP8 style. Continuing....")
-        os.system("exit 0")
+result = subprocess.check_output("python3 -m pylint /home/netman/Documents/Lab9/netman_netconf_obj2.py | grep \'Your code has been rated\' | awk \'{print $7}\' | awk -F/ \'{print $1}\'",shell=True)
+result = float(result)
+if result > 10:
+    print("PEP8 style code violation detected. Exiting....")
+    exit 1
+else:
+    print("Code complies with the PEP8 style. Continuing....")
+    exit 0
 
-
-if __name__ == '__main__':
-    check()
+#if __name__ == '__main__':
+#    check()
