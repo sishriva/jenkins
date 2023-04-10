71200c5d27fc44ec6515a44842f824fd3d88340e
sishriva <siddharth.shrivastava@colorado.edu>
2023-04-10 15:15:49 -0700

    Jenkins commit

--- checkPEP8.py
+++ checkPEP8.py
@@ -5,7 +5,7 @@ import os
 
 result = subprocess.check_output("python3 -m pylint /home/netman/Documents/Lab9/netman_netconf_obj2.py | grep \'Your code has been rated\' | awk \'{print $7}\' | awk -F/ \'{print $1}\'",shell=True)
 result = float(result)
-if result > 10:
+if result < 10:
     print("PEP8 style code violation detected. Exiting....")
     exit(1)
 else:
