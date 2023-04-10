b097c23619cbd329be502508d2155062a3466af0
sishriva <siddharth.shrivastava@colorado.edu>
2023-04-10 16:02:55 -0700

    Jenkins commit

--- checkPEP8.py
+++ checkPEP8.py
@@ -3,14 +3,16 @@
 import subprocess
 import os
 
+# result will store the score of pylint PEP8 code style
 result = subprocess.check_output("python3 -m pylint /home/netman/Documents/Lab9/netman_netconf_obj2.py | grep \'Your code has been rated\' | awk \'{print $7}\' | awk -F/ \'{print $1}\'",shell=True)
+
+#result will store the value in float
 result = float(result)
+
+#Jenkins should proceed further only if quality control is 5
 if result < 5:
-    print("PEP8 style code violation detected. Exiting....")
+    print("PEP8 style code violation detected. Fix the violations to continue. Exiting....")
     exit(1)
 else:
     print("Code complies with the PEP8 style. Continuing....")
     exit(0)
-
-#if __name__ == '__main__':
-#    check()
--- unitest.py
+++ unitest.py
@@ -3,28 +3,27 @@
 import unittest
 from netmiko import ConnectHandler
 
+# fetchip() to fetch the Loopback99 IP from R3 and test correct IP is configured
 def fetchip(org_IP):
     R3 = {"device_type" : 'cisco_ios','ip' : '198.51.100.13','username' : 'netman','password' : 'netman','secret' : 'netman', }
     net_connect = ConnectHandler(**R3)
     net_connect.enable()
     output = net_connect.send_command('sh ip int loop99')
-#    print(output)
     for line in output.split("\n"):
         if "Internet address" in line:
             ip = line.split()[-1]
-#            print(ip)
             break
     if org_IP == ip:
         return True
     else:
         return False
 
+# fetcharea() to fetch the number of OSPF areas configured on R1
 def fetcharea():
     R1 = {"device_type" : 'cisco_ios','ip' : '198.51.100.11','username' : 'netman','password' : 'netman','secret' : 'netman', }
     net_connect = ConnectHandler(**R1)
     net_connect.enable()
     output = net_connect.send_command('sh ip ospf | in area')
-#    print(output)
     for line in output.split("\n"):
         if "Number of areas in this router" in line:
             area = line.split()[-7]
@@ -35,7 +34,7 @@ def fetcharea():
     else:
         return False
             
-
+# pingR2R5() to test the reachability from R2 Loopback99 to R5 Loopback99
 def pingR2R5():
     R2 = {"device_type" : 'cisco_ios','ip' : '198.51.100.12','username' : 'netman','password' : 'netman','secret' : 'netman', }
     net_connect = ConnectHandler(**R2)
@@ -52,15 +51,18 @@ def pingR2R5():
         return False
 
 class TestConfiguration(unittest.TestCase):
+    
+    # testE3loop99ip() to run the unittest for R3 Loopback99 IP address
     def testR3loop99ip(self):
         print("\nTesting IP address of Loopback99 interface on R3 to 10.1.3.1/24....")
         self.assertTrue(fetchip("10.1.3.1/24"))
 
+    # testR1area() to run the unittest to check only one area is configured on R1
     def testR1area(self):
         print("\nTesting number of areas on R1....")
         self.assertTrue(fetcharea())
 
-
+    # testR2pingR5() to run the unittest to check the R2 Loopback99 reachability to R5 Loopback99
     def testR2pingR5(self):
         print("\nTesting reachability from R2 loopback to R5 loopback....")
         self.assertTrue(pingR2R5())
